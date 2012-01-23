42aa730efa8d1e1c47e649db97995871885e4cbc
matsuda <hi@matsuda.me>
2012-01-23 13:03:53 +0900

    最新のfacebook-ios-sdkに対応

--- FacebookLikeView/Classes/Facebook+ForceDialog.m
+++ FacebookLikeView/Classes/Facebook+ForceDialog.m
@@ -12,12 +12,10 @@
 @implementation Facebook(ForceDialog)
 
 - (void)authorize:(NSArray *)permissions
-         delegate:(id<FBSessionDelegate>)delegate {
+{
     // Hack to force Facebook SDK to always use in-app dialog for auth
     [_permissions release];
     _permissions = [permissions retain];
-
-    _sessionDelegate = delegate;
 
     objc_msgSend(self, @selector(authorizeWithFBAppAuth:safariAuth:), NO, NO);
 }
